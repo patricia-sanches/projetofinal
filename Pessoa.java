e3880f7aea17711c321a85b93ffefbd2d26e1118
patricia-sanches <pfasanches@gmail.com>
2025-04-25 17:51:15 -0300

    atualizacao pessoa

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+
+package inicio;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+    protected String nome;
+    protected String cpf;
+    protected LocalDate dataNascimento;
+
+    public Pessoa(String nome, String cpf, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+}
